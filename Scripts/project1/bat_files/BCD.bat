34837c614aa107af0a778dbe65a0390893e31457
hasmahdev <hasmahdev@gmail.com>
2025-11-01 14:51:05 +0300

    organized

--- Scripts/project1/bat_files/BCD.bat
+++
@@ -1,2 +0,0 @@
-start C:\Users\django1\.abcd\app\Scripts\project1\dist\first.exe
-
--- Scripts/project1/bat_files/run_app.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-result = subprocess.run(['C:\Users\django1\.abcd\project1\server.py'], capture_output=True, text=True)
